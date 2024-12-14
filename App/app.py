c81f59f2ba2cac326a1d09f575b46481031cba86
Hanife Kaptan <kptnhanife.2002@gmail.com>
2024-12-14 11:39:32 +0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,76 +1,65 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Car Prediction #
-# İkinci el araç fiyatlarını (özelliklerine göre) tahmin eden modeller oluşturma ve MLOPs ile Hugging Face üzerinden yayımlayacağız.
-
 import pandas as pd
-# from sklearn.model_selection import train_test_split #veri setini bölme işlemleri
-from sklearn.linear_model import LinearRegression #Doğrusal regresyon
-# from sklearn.metrics import r2_score,mean_squared_error #modelimizin performansını ölçmek için
-from sklearn.compose import ColumnTransformer #Sütun dönüşüm işlemleri
-from sklearn.preprocessing import OneHotEncoder, StandardScaler # kategori - sayısal dönüşüm  ve ölçeklendirme
-from sklearn.pipeline import Pipeline #Veri işleme hattı
+from sklearn.model_selection import train_test_split  # Splits the dataset
+from sklearn.linear_model import LinearRegression  # Sets up the linear regression model
+from sklearn.metrics import r2_score,mean_squared_error  # Measures the performance of the model
+from sklearn.compose import ColumnTransformer  # Performs column transformation operations
+from sklearn.preprocessing import OneHotEncoder, StandardScaler  # Performs transformation and scaling operations
+from sklearn.pipeline import Pipeline  # Data processing pipeline
+import streamlit as st  # Used for deploying the model
 
-df=pd.read_excel('cars.xls')
+data = pd.read_excel("cars.xls")
 
-X = df.drop("Price", axis=1) #bağımsız değişkenler
-y = df["Price"] #bağımlı değişken, tahmin edilecek değer
+X = data.drop("Price", axis=1)  # Independent variable
+y = df["Price"]  # Dependent variable
 
-# Veri ön işleme, standartlaştırma ve OHE işlemlerini otomatikleştiriyoruz (standarlaştırıyoruz).
-# Artık preprocess kullanarak kullanıcında arayüz aracılığıyla gelen veriyi modelimize uygun hale çevirebiliriz.
-preprocess=ColumnTransformer(
-    transformers=[('num',StandardScaler(),['Mileage', 'Cylinder','Liter','Doors']),
-        ('cat',OneHotEncoder(),['Make','Model','Trim','Type'])])
+# Data preprocessing, standardization, and One-Hot Encoding
+preprocess = ColumnTransformer(
+    transformers=[("num", StandardScaler(), ["Mileage", "Cylinder", "Liter", "Doors"]), # Standard scaler is applied to numeric data
+                  ("cat", OneHotEncoder(), ["Make", "Model", "Trim", "Type"])])  # One-hot encoding is applied to categorical data
 
-my_model=LinearRegression()
+LR = LinearRegression()
 
-#pipeline ı tanımla
-pipe=Pipeline(steps=[('preprocessor',preprocess),('model',my_model)]) # bu işlem ile web sitesinden alacağımız verilerin dönüşümü sağlanır.
+# Pipeline
+pipe = Pipeline(steps= [("preprocessor", preprocess), ("model", LR)])  # Ensures the transformation of data from the website
 
-# İyi bir tahmin sonucu verdiği için bütün veri setini eğitim için kullanacağız.
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# pipe.fit(X_train,y_train)  
-# y_pred=pipe.predict(X_test)
-# print('RMSE',mean_squared_error(y_test,y_pred)**0.5)
-# print('R2',r2_score(y_test,y_pred))
+# It may be preferable to use the entire dataset for training since it provides a good prediction result
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+# pipe.fit(X_train, y_train)  
+# y_pred = pipe.predict(X_test)
+# print("RMSE", mean_squared_error(y_test,y_pred, squared = False))
+# print("R2", r2_score(y_test, y_pred))
 
-pipe.fit(X,y)
+pipe.fit(X, y)
 
-
-# Streamlit ile modeli yayma/deploy/kullanıma sunma
-# Python ile yapılan çalışmnalrın hızlı bir şekilde deploy edilmesi için HTML render arayüzler tasarlamanızı sağlar.
-
-import streamlit as st
-#price tahmin fonksiyonu tanımla
+# Prediction function
 def price(mileage, make, model, trim, type, cylinder, liter, doors, cruise, sound, leather):
-    input_data= pd.DataFrame({"Mileage":[mileage],
-                              "Make":[make],
-                              "Model":[model],
-                              "Trim":[trim],
-                              "Type":[type],
-                              "Cylinder":[cylinder],
-                              "Liter":[liter],
-                              "Doors":[doors],
-                              "Cruise":[cruise],
-                              "Sound":[sound],
-                              "Leather":[leather]})
+    input_data= pd.DataFrame({"Mileage": [mileage],
+                              "Make": [make],
+                              "Model": [model],
+                              "Trim": [trim],
+                              "Type": [type],
+                              "Cylinder": [cylinder],
+                              "Liter": [liter],
+                              "Doors": [doors],
+                              "Cruise": [cruise],
+                              "Sound": [sound],
+                              "Leather": [leather]})
     prediction = pipe.predict(input_data)[0]
     return prediction
 
-st.title("2. El Araba Fiyatı Tahmin:red_car: @hanifekaptan")
-st.write('Arabanın özelliklerini seçiniz')
-mileage=st.number_input('Kilometre',100,200000)
-make=st.selectbox('Marka',df['Make'].unique())
-model=st.selectbox('Model',df[df['Make']==make]['Model'].unique())
-trim=st.selectbox('Trim',df[(df['Make']==make) &(df['Model']==model)]['Trim'].unique())
-car_type=st.selectbox('Araç Tipi',df[(df['Make']==make) &(df['Model']==model)&(df['Trim']==trim)]['Type'].unique())
-cylinder=st.selectbox('Cylinder',df['Cylinder'].unique())
-liter=st.number_input('Yakıt Hacmi',1,10)
-doors=st.selectbox('Kapı Sayısı',df['Doors'].unique())
-cruise=st.radio('Hız Sbt.',[True,False])
-sound=st.radio('Ses Sistemi',[True,False])
-leather=st.radio('Deri Döşeme.',[True,False])
-if st.button('Tahmin'):
+st.title("Used Car Price Prediction red_car")
+st.write("Please select the car features.")
+mileage = st.number_input("Mileage", 100, 200000)
+make=st.selectbox("Marka", df["Make"].unique())
+model=st.selectbox("Model", df[df["Make"] == make]["Model"].unique())
+trim=st.selectbox("Trim", df[(df["Make"] == make) &(df["Model"] == model)]["Trim"].unique())
+car_type=st.selectbox("Type", df[(df["Make"] == make) & (df["Model"] == model) & (df["Trim"] == trim)]["Type"].unique())
+cylinder=st.selectbox("Cylinder", df["Cylinder"].unique())
+liter=st.number_input("Fuel Volume", 1, 10)
+doors=st.selectbox("Doors", df['Doors'].unique())
+cruise=st.radio("Speed Constant", [True, False])
+sound=st.radio("Sound System", [True, False])
+leather=st.radio("Leather", [True, False])
+if st.button("Prediction"):
     pred=price(mileage, make, model, trim, car_type, cylinder, liter, doors, cruise, sound, leather)
-    st.write('Fiyat ($):', round(pred,2))
+    st.write("Price ($):", round(pred, 2))
